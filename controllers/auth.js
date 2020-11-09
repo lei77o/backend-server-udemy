a1e21b1332c3ee284a975cca1af495867cad5331
Leonardo Albrecht <albrecht.leo@gmail.com>
2020-11-09 16:07:23 -0300

    Seccion 12 - Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,9 @@ const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
+const usuario = require('../models/usuario');
+
 
 
 const login = async( req, res = response ) => {
@@ -47,10 +50,58 @@ const login = async( req, res = response ) => {
         })
     }
 
+}
+
+const googleSignIn = async(req, res = response )=>{
+
+    const googleToken = req.body.token;
+
+    try {
+
+        const { name, email, picture } = await googleVerify ( googleToken );
+
+        //Verificar si existe el usuario con ese email en la base de datos
+        const usuarioDB = await Usuario.findOne( {email} );
+        let usuario;
+        
+        if( !usuarioDB ){
+            usuario = new Usuario({
+                    nombre: name,
+                    email,
+                    password:'@@@',
+                    img: picture,
+                    google: true
+                
+                });
+        }
+        else {
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        //Guardar en Base de datos
+        await usuario.save();
+
+        //Generar JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            ok: true,
+            token
+        });
+
+        
+    } catch (error) {
+        
+        res.status(401).json({
+            ok: false,
+            msg: 'Token no encontrado',
+        })
+    }
 
 }
 
-
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
